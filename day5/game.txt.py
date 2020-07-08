fb60a02ae05820feb8e814ca11ce8ba83a05d657
Sahil <sahilmallickndp@gmail.com>
2020-07-08 14:22:16 +0530

    Day5 is added

---
+++ day5/game.txt.py
@@ -0,0 +1,10 @@
+"""
+1. list of words
+2. random word function
+3. number of  attempts
+4. alphabets and dashes
+5. an if-else block to check whether the alphabet entered is correct or not
+6. win or loss
+
+
+"""
---
+++ day5/ran.py
@@ -0,0 +1,11 @@
+import random
+
+
+#k=random.randint(1,100)
+#print(k)
+
+
+names=["Akanshu","Sahil","Argha","Aman","Ankita","Ambika","Pooja","Ayusha","Saquib","Kajal","Rhythm","Deepti","Srijita"]
+
+k=random.randint(0,len(names)-1)
+print(names[k])
